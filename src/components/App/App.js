47faee76b471e0ba85b7195c3c5ba32933a88103
ksavransky <ksavransky@gmail.com>
2017-12-12 22:59:42 -0800

    styling added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,10 +9,17 @@ class App extends Component {
     return (
       <div id='app' {...props}>
         <div id="app-header">
-          <h2>Markov Chain Tweets!</h2>
+          <h2>Markov Chain Tweets</h2>
         </div>
-        <button onClick={this.props.actions.twitterAuth}>Get Twitter Auth</button>
+        <div className='search-area'>
+        <h4>Search For A Twitter User To See Their Tweets</h4>
+        <div className='search-container grid-container grid-x align-center'>
+          <input type="search" placeholder='Search...' className='search cell small-6' />
+          <button className='button' onClick={this.props.actions.twitterAuth}>Search</button>
+        </div>
+        <h5>The Tweets Will Be Displayed Below In A <a target='_blank' href='https://en.wikipedia.org/wiki/Markov_chain'>Markov Chain</a></h5>
         <div className='results'>{this.props.results}</div>
+        </div>
       </div>
     );
   }
