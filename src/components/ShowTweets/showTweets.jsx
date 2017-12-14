7dcc067835bc0ad8ccba98e080029b0211ec4f8d
ksavransky <ksavransky@gmail.com>
2017-12-13 17:14:37 -0800

    refactor

--- src/components/ShowTweets/showTweets.jsx
+++ src/components/ShowTweets/showTweets.jsx
@@ -5,22 +5,27 @@ import './showTweets.css';
 
 class ShowTweets extends Component {
 
-constructor(props){
-  super(props)
-  this.state = {
-    tweetIds: null
+  constructor(props){
+    super(props)
+    this.state = {
+      tweetIds: null
+    }
   }
-}
 
 
   componentWillReceiveProps(nextProps){
     if (!_.isEqual(this.props, nextProps) && this.props.searchResults !== '[]'){
       this.setState({
-        // tweetIds: nextProps.searchResults.match(/id_str(.*)text/)
-        tweetIds: nextProps.searchResults.split('","text"')
-            .map(str => str.slice(-25).match(/[0-9]+/)).filter(el => el !== null ).map(ary => ary[0])
+        tweetIds: this.parseSearchResultForTweetIds(nextProps.searchResults)
       })
     }
+  }
+
+  parseSearchResultForTweetIds(result){
+    return result.split('","text"')
+    .map(str => str.slice(-25).match(/[0-9]+/))
+    .filter(el => el !== null )
+    .map(ary => ary[0])
   }
 
   getEmbeddedTweet(tweetId){
