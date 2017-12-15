7b5b04afab653ba3de3dad3f8db5b6b740b2f30a
ksavransky <ksavransky@gmail.com>
2017-12-14 22:28:22 -0800

    tweet embed working

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import ShowTweets from '../ShowTweets/showTweets.jsx'
+import TweetEmbed from 'react-tweet-embed'
 import './app.css';
 
 class App extends Component {
@@ -35,6 +36,7 @@ class App extends Component {
           <input ref='search' type="search" placeholder='Search...' className='search cell small-6'/>
           <button className='button' onClick={this.getTweets.bind(this)}>Search</button>
         </div>
+        <TweetEmbed id='940554567414091776' options={{conversation: 'none' }}></TweetEmbed>
         <h5>The Lastest Ten Available Tweets Will Be Displayed Below In A <a rel="noopener noreferrer" target='_blank' href='https://en.wikipedia.org/wiki/Markov_chain'>Markov Chain</a></h5>
         <ShowTweets searchResults={this.props.searchResults}/>
         </div>
