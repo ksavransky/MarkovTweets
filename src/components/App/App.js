2d71fbe630c11bcb1029071c48cf6b91d9dc2dcb
ksavransky <ksavransky@gmail.com>
2017-12-25 15:00:21 -0800

    rid of old stuff

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,53 +1,33 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
-import ShowTweets from '../ShowTweets/showTweets.jsx'
 import './app.css';
 
 class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      user: ''
+      test: ''
     }
+    this.getTweets = this.getTweets.bind(this)
   }
-  
+
   componentDidMount(){
-    document.addEventListener('keydown', this.enterSubmit.bind(this))
+    this.getTweets()
   }
-  
-  componentWillUnmount(){
-    document.removeEventListener('keydown', this.enterSubmit.bind(this))
-  }
-  
-  enterSubmit(event){
-    if(event.keyCode === 13) {
-      this.getTweets()
-    }
-  }
-  
+
   getTweets(){
-    this.props.actions.twitterSearch(this.refs.search.value === '' ? NOT_REAL_USER : this.refs.search.value)
+    this.props.actions.twitterSearch('realdonaldtrump')
   }
 
   render() {
+    console.log(this.props)
     return (
       <div id='app'>
-        <div id="app-header">
-          <h2>Markov Chain Tweets</h2>
-        </div>
-        <div className='page'>
-        <h4>Search For A Twitter User To See Their Tweets</h4>
-        <div className='search-container grid-container grid-x align-center'>
-          <input id='search-input' ref='search' type="search" placeholder='Search...' className='search cell small-6'/>
-          <button className='button' onClick={this.getTweets.bind(this)}>Search</button>
-        </div>
-        <ShowTweets actions={this.props.actions} searchResults={this.props.searchResults} user={this.state.user} menuItemSelected={this.state.menuItemSelected}/>
-        </div>
+        {this.props.searchResults}
       </div>
     );
   }
 }
 
-const NOT_REAL_USER = 'notrealman12345654312x'
 
 export default App;
