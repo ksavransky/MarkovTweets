1d936c020ed123ffa0ab385d04d56882a1e8f9f6
ksavransky <ksavransky@gmail.com>
2017-12-26 17:50:36 -0800

    showing tweets

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
+import ShowTweet from '../ShowTweet/showTweet.jsx'
 import _ from 'lodash'
 import './app.css';
 
@@ -7,7 +8,7 @@ class App extends Component {
   constructor(props){
     super(props)
     this.state = {
-      test: ''
+      results: []
     }
     this.getTweets = this.getTweets.bind(this)
   }
@@ -16,21 +17,38 @@ class App extends Component {
     this.getTweets()
   }
 
+
+  componentWillReceiveProps(nextProps){
+    if (!_.isEqual(this.props, nextProps) && this.props.searchResults !== '[]'){
+      // console.log(nextProps);
+      let tweetIds = []
+      if(nextProps.searchResults){
+        let parsedObj = JSON.parse(nextProps.searchResults)
+        console.log('parsedObj');
+        console.log(parsedObj);
+        _.forEach(parsedObj.statuses, tweetObj => {
+          tweetIds.push(tweetObj['id_str'])
+        })
+      }
+      console.log(tweetIds);
+      this.setState({
+        results: tweetIds
+      })
+    }
+  }
+
   getTweets(){
     this.props.actions.twitterSearch('realdonaldtrump')
   }
 
   render() {
-    console.log(this.props.searchResults);
-    // if(this.props.searchResults){
-    //   _.forEach(JSON.parse(this.props.searchResults), tweetObj => {
-    //     console.log(tweetObj);
-    //   })
-    // }
-
+    console.log('in render');
+    // console.log(this.props.searchResults);
+    console.log(this.state.results);
     return (
       <div id='app'>
         {this.props.searchResults}
+        {this.state.results.map((tweetId, tweetIndex) => <ShowTweet key={tweetIndex} tweetId={tweetId} tweetIndex={tweetIndex}/>)}
       </div>
     );
   }
